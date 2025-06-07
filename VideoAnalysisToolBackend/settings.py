63f5734445bd0695ec3811d3bb73f822f8c65b77
Alvin Wong <alvinwong@ufl.edu>
2025-06-07 12:50:33 -0400

    Making static package

--- VideoAnalysisToolBackend/settings.py
+++ VideoAnalysisToolBackend/settings.py
@@ -117,10 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/assets/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'dist' / 'assets',
+]
 
-static_dir = BASE_DIR / "static"
-STATICFILES_DIRS = [static_dir] if static_dir.exists() else []
+TEMPLATES[0]['DIRS'] = [BASE_DIR / 'dist']
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
--- VideoAnalysisToolBackend/urls.py
+++ VideoAnalysisToolBackend/urls.py
@@ -19,9 +19,8 @@ from django.http import JsonResponse
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
-def hello_world(req):
-	return JsonResponse({"message":"Hello, World"})
+from django.urls import path, re_path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('api/', include('app.urls')),
@@ -29,3 +28,8 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+
+urlpatterns += [
+    re_path(r'^.*$', TemplateView.as_view(template_name="index.html")),
+]
